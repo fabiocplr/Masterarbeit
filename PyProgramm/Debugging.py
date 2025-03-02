53c2ee65138a985c5b4809c0e872e93fff6b188f
fabiocplr <153329903+fabiocplr@users.noreply.github.com>
2025-03-02 16:33:58 +0100

    Update Debugging.py file size in version control

--- PyProgramm/Debugging.py
+++ PyProgramm/Debugging.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87568815dac8e523314a0121812e619159739f2e9b16f2e1dab2791a00670a74
-size 5524
+oid sha256:741dccd98dbd0c9469f3a4cabb65cc5ffeecc0e33df8f89106d3af17c996794b
+size 6222
